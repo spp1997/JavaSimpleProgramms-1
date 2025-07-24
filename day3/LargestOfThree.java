b78916b77cd518605ed129ee6cd5d500d344e3f1
spp1997 <suprhashi188797s@gmail.com>
2025-07-24 20:08:04 +0530

    Maximum of three numbers

---
+++ day3/LargestOfThree.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class LargestOfThree {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = input.nextInt();
+
+        System.out.print("Enter the third number: ");
+        int num3 = input.nextInt();
+
+        int largest;
+
+        if (num1 >= num2 && num1 >= num3) {
+            largest = num1;
+        } else if (num2 >= num1 && num2 >= num3) {
+            largest = num2;
+        } else {
+            largest = num3;
+        }
+
+        System.out.println("The largest number is: " + largest);
+
+        input.close();
+    }
+}
